52177b39cf06fa0658808daeabf23712d61b0f0c
Dani-XT <danielte98@gmail.com>
2023-06-22 01:15:47 -0400

    Actualizacion 22-06-23 1:15

--- inicio/js/estilo-inicio.js
+++ inicio/js/estilo-inicio.js
@@ -1,45 +1,32 @@
-/* -----------------------------------------------------------------------
-   TODO: Estilo Carrusel
-   ----------------------------------------------------------------------- */
-   $(document).ready(function () {
-    var owl = $(".owl-carousel");
-  
-    owl.owlCarousel({
-      loop: true,
-      margin: 10,
-      nav: true,
-      autoplay: true,
-      autoplayTimeout: 3000,
-      autoplayHoverPause: true,
-      center: true,
-      navText: [
-        "<i class='fa fa-angle-left'></i>",
-        "<i class='fa fa-angle-right'></i>",
-      ],
-      responsive: {
-        0: {
-          items: 1,
-        },
-        800: {
-          items: 3,
-        },
-        1000: {
-          items: 3,
-        },
+$(document).ready(function () {
+  var owl = $(".owl-carousel");
+
+  owl.owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    autoplay: true,
+    mouseDrag: true,
+    autoplayTimeout: 30000000,
+    autoplayHoverPause: true,
+    center: true,
+    navText: [
+      "<i class='fa fa-angle-left'></i>",
+      "<i class='fa fa-angle-right'></i>",
+    ],
+    responsive: {
+      0: {
+        items: 1,
       },
-    });
-  
-    
+      800: {
+        items: 3,
+      },
+      1000: {
+        items: 3,
+      },
+    },
   });
-  
-  
-  
-  
-  
-  
-  
-
-
+});
 
 /* -----------------------------------------------------------------------
    TODO: Estilo LOGIN
