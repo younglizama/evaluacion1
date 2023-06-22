df0a6fa805bc6e4fc40d62f0583001ef6e68125f
Dani-XT <danielte98@gmail.com>
2023-06-21 20:22:07 -0400

    Actualizacion 21-06-2023 20:22

--- inicio/js/estilo-inicio.js
+++ inicio/js/estilo-inicio.js
@@ -1,32 +1,47 @@
 /* -----------------------------------------------------------------------
    TODO: Estilo Carrusel
    ----------------------------------------------------------------------- */
-$(document).ready(function () {
-  $(".owl-carousel").owlCarousel({
-    loop: true,
-    margin: 10,
-    nav: true,
-    autoplay: true,
-    autoplayTimeout: 3000,
-    autoplayHoverPause: true,
-    center: true,
-    navText: [
-      "<i class='fa fa-angle-left'></i>",
-      "<i class='fa fa-angle-right'></i>",
-    ],
-    responsive: {
-      0: {
-        items: 1,
+   $(document).ready(function () {
+    var owl = $(".owl-carousel");
+  
+    owl.owlCarousel({
+      loop: true,
+      margin: 10,
+      nav: true,
+      autoplay: true,
+      autoplayTimeout: 3000,
+      autoplayHoverPause: true,
+      center: true,
+      navText: [
+        "<i class='fa fa-angle-left'></i>",
+        "<i class='fa fa-angle-right'></i>",
+      ],
+      responsive: {
+        0: {
+          items: 1,
+        },
+        800: {
+          items: 3,
+        },
+        1000: {
+          items: 3,
+        },
       },
-      800: {
-        items: 3,
-      },
-      1000: {
-        items: 3,
-      },
-    },
+    });
+  
+    // Imprimir "hola" por consola al seleccionar una imagen
+    $(".cont-img-carrusel").on("click", function () {
+      console.log("Hola");
+    });
   });
-});
+  
+  
+  
+  
+  
+  
+
+
 
 /* -----------------------------------------------------------------------
    TODO: Estilo LOGIN
