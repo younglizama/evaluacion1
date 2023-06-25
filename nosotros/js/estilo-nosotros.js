5746df801f034e0f830d2f303bde92322342978c
Dani-XT <danielte98@gmail.com>
2023-06-24 23:40:53 -0400

    Avance sobre nosotros

---
+++ nosotros/js/estilo-nosotros.js
@@ -0,0 +1,27 @@
+// $(document).ready(function () {
+//     let anchoPantalla = $(window).width();
+//     let 
+    
+
+
+//     if (anchoPantalla < 992) {
+
+//     }
+
+
+// //   let flagEjecutafun = true;
+
+// //   do {
+// //     switch (true) {
+        
+// //         case anchoPantalla < 992:
+// //             console.log("Menu 1: Pantalla del tamaño de " + anchoPantalla);
+// //             break;
+
+// //         case anchoPantalla < 500:
+// //             console.log("Menu 2: Pantalla de tamaño de "+ anchoPantalla)
+
+// //     }
+// //   } while (flagEjecutafun === false);
+
+// });
